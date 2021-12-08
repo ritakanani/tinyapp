c9b7be200689871b92c41dd56b014b16b65e81d4
Ritaben Vaghasia <ritakanani@yahoo.com>
2021-12-08 17:55:50 -0500

    tinyapp login and logout

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const PORT = 8080; // default port 8080
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 // set the view engine to ejs
 app.set("view engine", "ejs");
@@ -11,6 +12,7 @@ app.set("view engine", "ejs");
 //        MIDDLEWARE
 //
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser()); // bring in cookie-parser
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -27,51 +29,90 @@ function generateRandomString() {
   return result;
 }
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);      // output urlDatabse object in JSON string
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);      // output urlDatabse object in JSON string
+// });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");  // output In html page, "Hello (bold)World" inside body
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");  // output In html page, "Hello (bold)World" inside body
+// });
 
-app.get("/set", (req, res) => {
-  const a = 1;          
-  res.send(`a = ${a}`);           // output a = 1
- });
+// app.get("/set", (req, res) => {
+//   const a = 1;          
+//   res.send(`a = ${a}`);           // output a = 1
+//  });
  
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);        // error: a is not defined
- });
+//  app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}`);        // error: a is not defined
+//  });
+
 
  // index page
  app.get("/urls", (req, res) => {
-   const templateVars = { urls: urlDatabase};
+   const templateVars = { urls: urlDatabase, username: req.cookies["username"]};
+   
    res.render("urls_index", templateVars);
  });  // When sending variables to an EJS template, we need to send them inside an object, even if we are only sending one variable. This is so we can use the key of that variable (in the above case the key is urls) to access the data within our template.
 
+
  // get route to show the Form
  app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+    // ... any other vars
+  };
+  console.log(templateVars);
+  res.render("urls_new", templateVars);
+  
 });
+
+// //   LOG IN // //
+
+app.post("/login", (req, res) => {
+  console.log("username", req.body);  // from body username box, longURL is username 
+  res.cookie("username", req.body.longURL);
+  
+  res.redirect("/urls");
+});
+
+// // LOG OUT  // //
+
+app.post("/logout", (req, res) => {
+  let clearUsername = req.body.longURL;
+    res.clearCookie("username", clearUsername);
+    res.redirect("/urls");
+});
+
+
+// //      EDIT   // //
+
+app.post("/urls/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
+  let longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect("/urls");
+});
+
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   // res.send("Ok");
   // Respond with 'Ok' (we will replace this)
-  let longURL = req.body.longURL;
-  let shortURL = generateRandomString();
-  urlDatabase[shortURL] = longURL;
-  res.redirect(`/urls/${shortURL}`);
-});
+  let longURL = req.body.longURL;   // requesting data(longlink) from server
+  let shortURL = generateRandomString();   // 6 char shortURL called by function
+  urlDatabase[shortURL] = longURL;     // shortURL becomes key of urlDatabase and it's value is longURL
+  res.redirect(`/urls/${shortURL}`);  
+});  
+// When we submit the link redirect to /urls/${shortURL}
+
 
  // show shortURL and longURL page
  app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]  };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 // shorURL is stored the data in req.params in variable name. Like  http://localhost:8080/urls/b2xVn2 gives the result of longURL. 
@@ -83,11 +124,14 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+//  //  DELETE  //  //
+
 app.post("/urls/:shortURL/delete", (req, res) => { 
   let shortURL = req.params.shortURL;  
   delete urlDatabase[shortURL];  
   res.redirect("/urls");  
 });
+
 
 
 
